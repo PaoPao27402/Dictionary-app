24f515a2d90f9f3f80870bf99084b9d54ff31eaf
Paola Ruiloba <75644688+PaoPao27402@users.noreply.github.com>
2021-05-10 13:18:40 +0300

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -4,22 +4,23 @@ import './Search.css';
 
 export default function Search(){
     let [keyword, setKeyword] = useState("")
+
+    function handleResponse(response){
+        console.log(response.data[0]);
+
+    }
+
     function search(event){
         event.preventDefault();
         alert(`searching for ${keyword} definition`);
+
+      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+      console.log(apiUrl);
+     axios.get(apiUrl).then(handleResponse);  
     }
-
-    function handleResponse(response){
-        console.log(response);
-
-    }
-
-     let apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en_US/hello";
-     axios.get(apiUrl).then(handleResponse);
 
     function handleKeywordChange(event){
         setKeyword(event.target.value);
-
     }
 
     return(
