cdcd7c5a7352406f3e7476061d48c956a805c649
Paola Ruiloba <75644688+PaoPao27402@users.noreply.github.com>
2021-05-19 13:26:09 +0300

    update app css + phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,8 +4,9 @@ import React from "react";
 export default function Phonetic(props){
     return (
         <div className="Phonetic">
+            🔊
             <a href={props.phonetic.audio} target="_blank" rel="noreferrer"> 
-            🔊 {props.phonetic.text}
+             {props.phonetic.text}
             </a>
         </div>
     );
